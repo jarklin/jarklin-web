3ed82dba532e009c9c75cc7932de2777bb20f5af
PlayerG9 <g9yout@gmail.com>
2024-01-06 23:02:30 +0100

    configured react-router

--- src/web-ui/src/App.tsx
+++ src/web-ui/src/App.tsx
@@ -1,5 +1,26 @@
+import { Routes, Route, Outlet } from "react-router-dom";
+
 export default function App() {
     return <>
-        <h1 className="text-center text-2xl">Hello Jarklin</h1>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Index />} />
+                <Route path="*" element={<p>404 Not Found</p>} />
+            </Route>
+        </Routes>
+    </>;
+}
+
+
+function Layout() {
+    return <>
+        <Outlet />
+    </>;
+}
+
+
+function Index() {
+    return <>
+        <h1 className="text-center text-2xl">Hello Jarklin!</h1>
     </>;
 }
