97d068791fc88d2c50a084b1e15ed7b73ab54820
PlayerG9 <g9yout@gmail.com>
2024-01-12 12:14:37 +0100

    added axios

--- src/web-ui/src/main.tsx
+++ src/web-ui/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
 import App from './App.tsx';
 import './index.css';
+import axios from "axios";
+
+
+axios.defaults.withCredentials = true;
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
