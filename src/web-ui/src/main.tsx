82c4ade8fc1ad60999db2cb97f0e2895393ce909
PlayerG9 <g9yout@gmail.com>
2024-01-12 12:20:30 +0100

    added react-query

--- src/web-ui/src/main.tsx
+++ src/web-ui/src/main.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
 import App from './App.tsx';
-import './index.css';
 import axios from "axios";
+import { QueryClient, QueryClientProvider } from "react-query";
+import './index.css';
 
 
 axios.defaults.withCredentials = true;
 
+const queryClient = new QueryClient();
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <QueryClientProvider client={queryClient}>
+            <HashRouter>
+                <App />
+            </HashRouter>
+        </QueryClientProvider>
     </React.StrictMode>,
 );
