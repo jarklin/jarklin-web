c004f0a18173a602a3d08795ac2e761f08641a9c
PlayerG9 <g9yout@gmail.com>
2024-01-21 12:56:51 +0100

    improved retrying for react-query

--- src/web-ui/src/main.tsx
+++ src/web-ui/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
 import App from './App.tsx';
-import axios from "axios";
+import axios, { AxiosError, HttpStatusCode } from "axios";
 import { QueryClient, QueryClientProvider } from "react-query";
 import './index.css';
 
@@ -10,7 +10,27 @@ import './index.css';
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "./";  // todo: validate if this works with changed server baseurl
 
-const queryClient = new QueryClient();
+const retryStatus: Array<number> = [
+    HttpStatusCode.RequestTimeout,
+    HttpStatusCode.TooEarly,
+    HttpStatusCode.TooManyRequests,
+    HttpStatusCode.ServiceUnavailable,
+];
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: (failureCount, error) => {
+                if (error instanceof AxiosError) {
+                    if (retryStatus.includes(error.status ?? 0)) {
+                        return failureCount < 5;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+});
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
