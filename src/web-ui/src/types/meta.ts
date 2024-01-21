454d199c7e627ca0c2a42e3f5f20ea9cfea2ff71
PlayerG9 <g9yout@gmail.com>
2024-01-21 11:37:38 +0100

    added types for jarklin-meta

---
+++ src/web-ui/src/types/meta.ts
@@ -0,0 +1,54 @@
+export type JarklinInfo = Array<JarklinInfoEntry>;
+
+export interface JarklinInfoEntry {
+    path: string,
+    name: string,
+    ext: string,
+    meta: VideoMeta | GalleryMeta,
+}
+
+
+
+export interface VideoMeta {
+    type: "video",
+    width: number,
+    height: number,
+    duration: number,
+    filesize: number,
+    n_previews: number,
+    video_streams: Array<{
+        is_default: boolean,
+        width: number,
+        height: number,
+        duration: number,
+        avg_fps: number,
+    }>,
+    audio_streams: Array<{
+        is_default: boolean,
+        language: string,
+    }>,
+    subtitles: Array<{
+        is_default: boolean,
+        language: string,
+    }>,
+    chapters: Array<{
+        id: number,
+        start: number,
+        start_time: number,
+        end: number,
+        end_time: number,
+        title: string,
+    }>,
+}
+
+export interface GalleryMeta {
+    type: "gallery",
+    n_previews: number,
+    images: Array<{
+        filename: string,
+        width: number,
+        height: number,
+        filesize: number,
+        is_animated: boolean,
+    }>,
+}
