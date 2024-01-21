50bfb0d196b449fcdf348913f5f64693b8dfa42e
PlayerG9 <g9yout@gmail.com>
2024-01-21 12:58:09 +0100

    added suspense and basic loading indicator for the page-layout

--- src/web-ui/src/PageLayout/index.tsx
+++ src/web-ui/src/PageLayout/index.tsx
@@ -3,17 +3,31 @@ import {ErrorBoundaryFallback} from "~/components/ErrorBoundaryFallback.tsx";
 import {Outlet} from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import {Suspense} from "react";
 
 export default function Index() {
     return <>
+        {/* fatal error */}
         <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
             <div className="flex flex-col h-screen">
                 <NavBar />
                 <main className="grow">
-                    <Outlet />
+                    {/* error in requests or so */}
+                    <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
+                        <Suspense fallback={<Loading />}>
+                            <Outlet />
+                        </Suspense>
+                    </ErrorBoundary>
                 </main>
                 <Footer />
             </div>
         </ErrorBoundary>
     </>;
+}
+
+
+function Loading() {
+    return <div className="h-full grid place-content-center">
+        <div className="h-[min(5vh,5vw)] aspect-square border-4 border-accent-light rounded-full border-t-transparent border-b-transparent animate-spin" />
+    </div>;
 }
