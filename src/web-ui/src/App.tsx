0f8b4945dffb9c06f7ff04a8e0d2cefcf3d60afb
PlayerG9 <g9yout@gmail.com>
2024-01-21 12:56:27 +0100

    added login/logout routes and error-reset on path-change

--- src/web-ui/src/App.tsx
+++ src/web-ui/src/App.tsx
@@ -1,17 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route, useLocation} from "react-router-dom";
 import HomePage from "~/pages/home";
+import LoginPage from "~/pages/login.tsx";
+import LogoutPage from "~/pages/logout.tsx";
 import NotFound from "~/pages/404.tsx";
 import PageLayout from "~/PageLayout";
 
 
 export default function App() {
+    const { pathname } = useLocation();
+
     return <>
-        <Routes>
+        <Routes key={pathname}>
             <Route element={<PageLayout />}>
                 <Route index element={<HomePage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="logout" element={<LogoutPage />} />
                 <Route path="*" element={<NotFound />} />
             </Route>
         </Routes>
     </>;
 }
-
