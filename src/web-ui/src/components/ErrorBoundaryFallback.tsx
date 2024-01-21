c3300986718968615b39d61982068cdb68a63d07
PlayerG9 <g9yout@gmail.com>
2024-01-21 12:57:21 +0100

    added navigation to login-page if error-boundary catches axios-error-unauthorized

--- src/web-ui/src/components/ErrorBoundaryFallback.tsx
+++ src/web-ui/src/components/ErrorBoundaryFallback.tsx
@@ -1,9 +1,15 @@
 import { FallbackProps } from "react-error-boundary";
+import {AxiosError, HttpStatusCode} from "axios";
+import {Navigate} from "react-router-dom";
 
 
 export function ErrorBoundaryFallback({ error }: FallbackProps) {
+    if (error instanceof AxiosError && error.status === HttpStatusCode.Unauthorized) {
+        return <Navigate to="/login" />;
+    }
+
     return <>
-        <div className="grid place-content-center w-screen h-screen">
+        <div className="grid place-content-center h-full">
             <div className="max-w-xs text-center p-2 bg-primary-light border border-red-500 rounded-lg">
                 <h1 className="text-2xl">Something went wrong!</h1>
                 <p className="opacity-50 text-xs">({error.name}: {error.message})</p>
