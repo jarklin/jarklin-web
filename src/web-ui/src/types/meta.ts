edc69fc8ee24b60174ffc40f5b35125b6a421017
PlayerG9 <g9yout@gmail.com>
2024-01-23 20:41:15 +0100

    added new mtime field (and code reformat) in meta.ts

--- src/web-ui/src/types/meta.ts
+++ src/web-ui/src/types/meta.ts
@@ -1,54 +1,55 @@
 export type JarklinInfo = Array<JarklinInfoEntry>;
 
 export interface JarklinInfoEntry {
-    path: string,
-    name: string,
-    ext: string,
-    meta: VideoMeta | GalleryMeta,
+    path: string
+    name: string
+    ext: string
+    mtime: number
+    meta: VideoMeta | GalleryMeta
 }
 
 
 
 export interface VideoMeta {
-    type: "video",
-    width: number,
-    height: number,
-    duration: number,
-    filesize: number,
-    n_previews: number,
+    type: "video"
+    width: number
+    height: number
+    duration: number
+    filesize: number
+    n_previews: number
     video_streams: Array<{
-        is_default: boolean,
-        width: number,
-        height: number,
-        duration: number,
-        avg_fps: number,
+        is_default: boolean
+        width: number
+        height: number
+        duration: number
+        avg_fps: number
     }>,
     audio_streams: Array<{
-        is_default: boolean,
-        language: string,
+        is_default: boolean
+        language: string
     }>,
     subtitles: Array<{
-        is_default: boolean,
-        language: string,
+        is_default: boolean
+        language: string
     }>,
     chapters: Array<{
-        id: number,
-        start: number,
-        start_time: number,
-        end: number,
-        end_time: number,
-        title: string,
+        id: number
+        start: number
+        start_time: number
+        end: number
+        end_time: number
+        title: string
     }>,
 }
 
 export interface GalleryMeta {
-    type: "gallery",
-    n_previews: number,
+    type: "gallery"
+    n_previews: number
     images: Array<{
-        filename: string,
-        width: number,
-        height: number,
-        filesize: number,
-        is_animated: boolean,
+        filename: string
+        width: number
+        height: number
+        filesize: number
+        is_animated: boolean
     }>,
 }
