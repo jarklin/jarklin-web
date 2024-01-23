616c0351d28399476e8e88da391a10b703920f62
PlayerG9 <g9yout@gmail.com>
2024-01-23 11:41:02 +0100

    accepted meta in MetaCard sub-components for video/gallery

--- src/web-ui/src/components/MetaCard/gallery.tsx
+++ src/web-ui/src/components/MetaCard/gallery.tsx
@@ -1,13 +1,17 @@
 import type { GalleryMeta } from "~/types";
+import {twMerge} from "tailwind-merge";
 
 
 interface Props {
-    meta: GalleryMeta,
+    className?: string
+    meta: GalleryMeta
 }
 
 
-export default function GalleryCard({ meta }: Props) {
-    return <div>
+export default function GalleryCard(props: Props) {
+    const meta = props.meta;
+
+    return <div className={twMerge("", props.className)}>
         {JSON.stringify(meta)}
     </div>;
 }
--- src/web-ui/src/components/MetaCard/video.tsx
+++ src/web-ui/src/components/MetaCard/video.tsx
@@ -1,13 +1,17 @@
 import type { VideoMeta } from "~/types";
+import {twMerge} from "tailwind-merge";
 
 
 interface Props {
-    meta: VideoMeta,
+    className?: string
+    meta: VideoMeta
 }
 
 
-export default function VideoCard({ meta }: Props) {
-    return <div>
+export default function VideoCard(props: Props) {
+    const meta = props.meta;
+
+    return <div className={twMerge("", props.className)}>
         {JSON.stringify(meta)}
     </div>;
 }
