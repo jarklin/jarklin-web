6e8fcfbea040243b807e881edec0676b0815bee6
PlayerG9 <g9yout@gmail.com>
2024-01-29 10:07:33 +0100

    code reformat

--- src/web-ui/src/components/InfoCard/gallery.tsx
+++ src/web-ui/src/components/InfoCard/gallery.tsx
@@ -16,6 +16,6 @@ export default function GalleryCard(props: Props) {
     return <div className={twMerge("aspect-vertical bg-primary-light rounded-md overflow-hidden relative group", props.className)}>
         <img className="h-full w-full object-cover" src={getPreview(info.path)} alt=""/>
         <GalleryVerticalEndIcon className="absolute top-1 left-1" />
-        <span className="absolute inset-x-0 bottom-0 px-1 bg-black bg-opacity-50">{formatFilename(info.name)}</span>
+        <span className="absolute inset-x-0 bottom-0 px-1 bg-black/50">{formatFilename(info.name)}</span>
     </div>;
 }
--- src/web-ui/src/components/InfoCard/video.tsx
+++ src/web-ui/src/components/InfoCard/video.tsx
@@ -16,6 +16,6 @@ export default function VideoCard(props: Props) {
     return <div className={twMerge("aspect-video bg-primary-light rounded-md overflow-hidden relative group", props.className)}>
         <img className="h-full w-full object-cover" src={getPreview(info.path)} alt="" />
         <FilmIcon className="absolute top-1 left-1" />
-        <span className="absolute inset-x-0 bottom-0 px-1 bg-black bg-opacity-50">{formatFilename(info.name)}</span>
+        <span className="absolute inset-x-0 bottom-0 px-1 bg-black/50">{formatFilename(info.name)}</span>
     </div>;
 }
