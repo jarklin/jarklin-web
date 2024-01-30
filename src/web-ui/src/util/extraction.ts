0cbd1a4f0029c51158441f016e657c224edc761d
PlayerG9 <g9yout@gmail.com>
2024-01-30 11:32:54 +0100

    updated tag-extraction

--- src/web-ui/src/util/extraction.ts
+++ src/web-ui/src/util/extraction.ts
@@ -8,11 +8,11 @@ export function extractTags(path: string): Array<string> {
     // split by '/'|'\'
     const parts = path.split(/[\/\\]/g).filter(e => e);
 
-    // every element is added once (except the filename and folder name (last part))
-    tags.push(...parts.slice(0, parts.length - 2));
+    // // every element is added once (except the filename and folder name (last part))
+    // tags.push(...parts.slice(0, parts.length - 2));
 
     // add variations/combinations
-    for(let max = 2; max <= parts.length - 1; max++) {
+    for(let max = 1; max < parts.length; max++) {
         tags.push(parts.slice(0, max).join("|"))
     }
 
