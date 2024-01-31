db251af18d40c564f387af84cd7a0e76075e7a1d
PlayerG9 <g9yout@gmail.com>
2024-01-31 16:03:08 +0100

    updated VideoPlayerGestures

--- src/web-ui/src/components/VideoPlayer/gestures.tsx
+++ src/web-ui/src/components/VideoPlayer/gestures.tsx
@@ -3,9 +3,16 @@ import {Gesture} from "@vidstack/react";
 
 export default function VideoPlayerGestures() {
     return <>
+        {/*<Gesture*/}
+        {/*    className="absolute inset-0 z-0 block h-full w-full"*/}
+        {/*    event="pointerup"*/}
+        {/*    action="toggle:controls"*/}
+        {/*/>*/}
         <Gesture
-            // className="absolute inset-0 z-0 block h-full w-full"
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 block h-1/6 w-1/6"
+            /* this one is anoying on mobile */
+            className="absolute inset-0 z-0 block h-full w-full"
+            /* but this one doesn't work quite */
+            // className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 block h-1/6 w-1/6"
             event="pointerup"
             action="toggle:paused"
         />
