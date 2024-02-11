9ba320848d5612769c28c8b1d94a23b51adc54f5
PlayerG9 <g9yout@gmail.com>
2024-02-11 14:36:23 +0100

    separated LoadingSpinner to own component and fixed height-bug

--- src/web-ui/src/layouts/SimpleLayout/index.tsx
+++ src/web-ui/src/layouts/SimpleLayout/index.tsx
@@ -4,6 +4,7 @@ import {Outlet} from "react-router-dom";
 import NavBar from "./NavBar.tsx";
 import Footer from "../SimpleLayout/Footer.tsx";
 import {Suspense} from "react";
+import LoadingSpinner from "~/components/LoadingSpinner.tsx";
 
 interface Props {
     title: string
@@ -13,12 +14,12 @@ export default function Index(props: Props) {
     return <>
         {/* fatal error */}
         <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
-            <div className="flex flex-col h-screen">
+            <div className="flex flex-col h-full">
                 <NavBar title={props.title} />
                 <main className="grow">
                     {/* error in requests or so */}
                     <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
-                        <Suspense fallback={<Loading />}>
+                        <Suspense fallback={<LoadingSpinner className="h-full" />}>
                             <Outlet />
                         </Suspense>
                     </ErrorBoundary>
@@ -27,11 +28,4 @@ export default function Index(props: Props) {
             </div>
         </ErrorBoundary>
     </>;
-}
-
-
-function Loading() {
-    return <div className="h-full grid place-content-center">
-        <div className="h-[min(5vh,5vw)] aspect-square border-4 border-accent-light rounded-full border-t-transparent border-b-transparent animate-spin" />
-    </div>;
 }
