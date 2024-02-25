609cd6e4e39b34b63636a11aa1c4beea7cf3b5c0
PlayerG9 <g9yout@gmail.com>
2024-02-25 13:29:39 +0100

    slightly changed style of config/problems

--- src/web-ui/src/pages/config/problems/index.tsx
+++ src/web-ui/src/pages/config/problems/index.tsx
@@ -6,11 +6,12 @@ export default function ConfigProblemsPage() {
 
     return <>
         <h1 className="text-2xl">Problems</h1>
-        <p>Some Media could not be processed. See here the reasons:</p>
+        <p>Some Media could not be processed. See here the details:</p>
         <div className="grow flex flex-col gap-2">
             {problems.map(problem => <>
-                <div className="group font-mono bg-primary-light p-2 rounded-md">
-                    <p>File: {problem.file}</p>
+                <div className="group font-mono bg-primary-light p-2 rounded-md text-sm">
+                    <p className="text-accent">{problem.file}</p>
+                    <div className="h-px bg-primary/50"/>
                     <details>
                         <summary className="cursor-pointer">{problem.type}: {problem.description}</summary>
                         <pre className="text-sm bg-primary p-1 rounded-md overflow-x-scroll">{problem.traceback}</pre>
