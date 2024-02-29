80eb205e7a6c95742cffcab4f147c28908c557c4
PlayerG9 <g9yout@gmail.com>
2024-02-29 07:51:04 +0100

    improved ScrollToMe.tsx

--- src/web-ui/src/components/ScrollToMe.tsx
+++ src/web-ui/src/components/ScrollToMe.tsx
@@ -1,3 +1,35 @@
+import {useCallback, useEffect, useRef} from "react";
+
+interface Props {
+    if?: boolean
+    behavior?: "smooth" | "instant"
+}
+
+
+export default function ScrollToMe(props: Props) {
+    const ref = useRef<HTMLDivElement>(null);
+    const shouldScroll = props.if !== false;
+
+    const attemptScroll = useCallback((attempt: number = 0) => {
+        if (attempt >= 3) { return; }
+        if (ref.current) {
+            ref.current.scrollIntoView({ behavior: props.behavior ?? "smooth", block: "start" });
+        } else {
+            setTimeout(() => attemptScroll(attempt + 1), 100);
+        }
+    }, [ref]);
+
+    useEffect(() => {
+        if (shouldScroll) {
+            attemptScroll();
+        }
+    }, [shouldScroll, attemptScroll]);
+
+    return <div ref={ref} className="invisible" />;
+}
+
+// this version below somehow does not always work on mobile
+/*
 import {useEffect, useRef} from "react";
 
 interface Props {
@@ -18,3 +50,4 @@ export default function ScrollToMe(props: Props) {
 
     return <div ref={ref} className="invisible" />;
 }
+*/
