18aa2ada93eed6ba2362c74a9447f490d392d442
PlayerG9 <g9yout@gmail.com>
2024-03-01 13:37:08 +0100

    created LabelBox component

---
+++ src/web-ui/src/components/LabelBox.tsx
@@ -0,0 +1,13 @@
+import {twMerge} from "tailwind-merge";
+import {PropsWithChildren} from "react";
+
+
+interface Props extends PropsWithChildren {
+    className?: string
+    // style: "none" | "default" | keyof styles
+}
+
+
+export default function LabelBox(props: Props) {
+    return <span className={twMerge("px-2 py-px bg-accent text-primary rounded-full cursor-default", props.className)}>{props.children}</span>;
+}
