fbc950570a6032800cf2fffa13b51daeb1860746
PlayerG9 <g9yout@gmail.com>
2024-03-17 23:23:38 +0100

    added missing key to fragment

--- src/web-ui/src/pages/panel/index.tsx
+++ src/web-ui/src/pages/panel/index.tsx
@@ -1,4 +1,5 @@
 import {NavLink, Outlet} from "react-router-dom";
+import React from "react";
 
 
 const links = [
@@ -12,13 +13,13 @@ export default function ConfigPage() {
     return <>
         <div className="flex flex-col h-full gap-2 p-4 mx-auto max-w-screen-2xl">
             <div className="flex justify-center gap-4 p-2">
-                {links.map((link, i) => <>
+                {links.map((link, i) => <React.Fragment key={link.href}>
                     {i !== 0 && <div className="bg-accent/20 w-px h-5 my-auto" />}
                     <NavLink className="group p-1 [&.active>span]:bg-accent-light [&.active>span]:max-w-full" to={link.href}>
                         {link.label}
                         <span className="block mx-auto max-w-0 group-hover:max-w-full transition-[max-width] h-px bg-accent" />
                     </NavLink>
-                </>)}
+                </React.Fragment>)}
             </div>
             <Outlet />
         </div>
