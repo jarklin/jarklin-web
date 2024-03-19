e93eb7a9517b3dec31f2bb78af4bb7270803bcfc
PlayerG9 <g9yout@gmail.com>
2024-03-19 14:51:06 +0100

    replaced icon of GalleryCard and added proper sizing to MediaCards

--- src/web-ui/src/components/MediaCard/gallery.tsx
+++ src/web-ui/src/components/MediaCard/gallery.tsx
@@ -1,7 +1,7 @@
 import type {GalleryMediaEntry} from "~/types/media.ts";
 import {twMerge} from "tailwind-merge";
 import {getPreviewImage} from "~/util";
-import {GalleryVerticalEndIcon} from "lucide-react";
+import {ImagesIcon} from "lucide-react";
 import Image from "~/components/Image.tsx";
 
 
@@ -16,7 +16,7 @@ export default function GalleryCard(props: Props) {
 
     return <div className={twMerge("aspect-portrait bg-primary-light rounded-md overflow-hidden relative isolate group", props.className)}>
         <Image className="h-full w-full object-cover" loading="lazy" src={getPreviewImage(media.path)} />
-        <GalleryVerticalEndIcon className="absolute top-1 left-1 drop-shadow-highlight z-10" />
+        <ImagesIcon className="absolute top-1 left-1 h-5 w-5 drop-shadow-highlight z-10" />
         <span className="absolute inset-x-0 bottom-0 px-1 bg-gradient-to-t from-black/50 via-black/50 to-transparent drop-shadow-highlight line-clamp-3 z-10">{media.displayName}</span>
     </div>;
 }
--- src/web-ui/src/components/MediaCard/video.tsx
+++ src/web-ui/src/components/MediaCard/video.tsx
@@ -16,7 +16,7 @@ export default function VideoCard(props: Props) {
 
     return <div className={twMerge("aspect-video bg-primary-light rounded-md overflow-hidden relative isolate group", props.className)}>
         <Image className="h-full w-full object-cover" loading="lazy" src={getPreviewImage(media.path)} />
-        <FilmIcon className="absolute top-1 left-1 drop-shadow-highlight z-10" />
+        <FilmIcon className="absolute top-1 left-1 h-5 w-5 drop-shadow-highlight z-10" />
         <span className="absolute inset-x-0 bottom-0 px-1 bg-gradient-to-t from-black/50 via-black/50 to-transparent drop-shadow-highlight line-clamp-2 z-10">{media.displayName}</span>
     </div>;
 }
