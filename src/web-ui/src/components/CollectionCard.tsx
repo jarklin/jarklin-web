fe7e21b41dedd72c991d7b49a03ee0dfb3cc37b4
playerg9 <g9yout@gmail.com>
2024-03-24 10:26:15 +0100

    updated MediaCards

--- src/web-ui/src/components/CollectionCard.tsx
+++ src/web-ui/src/components/CollectionCard.tsx
@@ -14,8 +14,8 @@ export default function CollectionCard(props: Props) {
     const { collection } = props;
 
     return <div className={twMerge("aspect-square bg-primary-light rounded-md overflow-hidden relative isolate group", props.className)}>
-        <Image className="h-full w-full object-cover" loading="lazy" src={collection.previewUrl} />
-        <LibrarySquareIcon className="absolute top-1 left-1 h-5 w-5 drop-shadow-highlight z-10" />
-        <span className="absolute inset-x-0 bottom-0 px-1 bg-gradient-to-t from-black/50 via-black/50 to-transparent drop-shadow-highlight line-clamp-3 z-10">{collection.displayName}</span>
+        <Image className="size-full object-cover" loading="lazy" src={collection.previewUrl} />
+        <LibrarySquareIcon className="absolute top-1 left-1 size-5 drop-shadow-highlight z-10" />
+        <span className="absolute inset-x-0 bottom-0 px-1 bg-gradient-to-t from-75% from-black/50 drop-shadow-highlight line-clamp-3 z-10">{collection.displayName}</span>
     </div>;
 }
--- src/web-ui/src/components/MediaCard/video.tsx
+++ src/web-ui/src/components/MediaCard/video.tsx
@@ -15,8 +15,8 @@ export default function VideoCard(props: Props) {
     const { media } = props;
 
     return <div className={twMerge("aspect-video bg-primary-light rounded-md overflow-hidden relative isolate group", props.className)}>
-        <Image className="h-full w-full object-cover" loading="lazy" src={getPreviewImage(media.path)} />
-        <FilmIcon className="absolute top-1 left-1 h-5 w-5 drop-shadow-highlight z-10" />
-        <span className="absolute inset-x-0 bottom-0 px-1 bg-gradient-to-t from-black/50 via-black/50 to-transparent drop-shadow-highlight line-clamp-2 z-10">{media.displayName}</span>
+        <Image className="size-full object-cover" loading="lazy" src={getPreviewImage(media.path)} />
+        <FilmIcon className="absolute top-1 left-1 size-5 drop-shadow-highlight z-10" />
+        <span className="absolute inset-x-0 bottom-0 px-1 bg-gradient-to-t from-75% from-black/50 drop-shadow-highlight line-clamp-2 z-10">{media.displayName}</span>
     </div>;
 }
