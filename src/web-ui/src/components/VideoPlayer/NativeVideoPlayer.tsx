9611e34393a065082b13eb05e7579d623134d74b
playerg9 <g9yout@gmail.com>
2024-03-29 16:25:39 +0100

    improved custom player with chapters track

--- src/web-ui/src/components/VideoPlayer/NativeVideoPlayer.tsx
+++ src/web-ui/src/components/VideoPlayer/NativeVideoPlayer.tsx
@@ -1,6 +1,8 @@
 import type {VideoMediaEntry} from "~/types";
-import {getSource} from "~/util";
+import {getSource, VttCreator} from "~/util";
 import {twMerge} from "tailwind-merge";
+import {useMemo} from "react";
+import {extractChapterInformation} from "~/components/VideoPlayer/common.ts";
 
 
 interface Props {
@@ -10,10 +12,34 @@ interface Props {
 
 
 export default function NativeVideoPlayer(props: Props) {
+    const { media } = props;
+
+    const chapters = useMemo(() => extractChapterInformation(media), [media]);
+    const chaptersVTTUrl = useMemo(() => generateVttUrl(chapters), [chapters]);
+
     return <video
         className={twMerge("size-full bg-black", props.className)}
         controls autoPlay autoFocus disablePictureInPicture disableRemotePlayback
     >
-        <source src={getSource(props.media.path)} />
+        <source src={getSource(media.path)} />
+        <track default kind="chapters" src={chaptersVTTUrl} srcLang="en" label="Chapters" />
+        {/*{media.meta.subtitles.map(subtitle => (*/}
+        {/*    <track key={subtitle.language} kind="subtitles" srcLang={subtitle.language} label={subtitle.language} />*/}
+        {/*))}*/}
     </video>;
+}
+
+
+function generateVttUrl(chapters: ReturnType<typeof extractChapterInformation>) {
+    const vtt = new VttCreator();
+    for (const chapter of chapters) {
+        vtt.add(chapter.startTime, chapter.endTime, chapter.title);
+    }
+    const content = vtt.toString();
+    const bytes = new TextEncoder().encode(content);
+    const blob = new Blob([bytes], {
+        type: "text/vtt",
+    });
+    return URL.createObjectURL(blob);
+    // todo: somehow URL.revokeObjectURL()
 }
