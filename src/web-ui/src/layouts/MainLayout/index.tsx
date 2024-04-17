b4d4672fdf14c90906253ce4a9b392d32b72af15
playerg9 <g9yout@gmail.com>
2024-04-17 11:05:59 +0200

    removed buggy css

--- src/web-ui/src/layouts/MainLayout/index.tsx
+++ src/web-ui/src/layouts/MainLayout/index.tsx
@@ -19,7 +19,8 @@ export default function MainLayout(props: Props) {
             <div className="flex flex-col min-h-screen">
                 <NavBar title={props.title} />
                 {/* don't ask why. but this h-1 is necessary for children to use h-full */}
-                <main className="h-1 grow">
+                {/*<main className="h-1 grow">*/}
+                <main className="grow">
                     {/* error in requests or so */}
                     <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
                         <Suspense fallback={<LoadingSpinner className="h-full" />}>
