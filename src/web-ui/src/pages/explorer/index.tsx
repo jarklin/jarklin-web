04521103be941581c4070895beff1f0766298506
playerg9 <g9yout@gmail.com>
2024-07-24 23:19:21 +0200

    fixed fucking bad javascript string.replace

--- src/web-ui/src/pages/explorer/index.tsx
+++ src/web-ui/src/pages/explorer/index.tsx
@@ -22,7 +22,7 @@ export default function ExplorerPage() {
 
     const directories: string[] = useMemo(
         () => allTags
-                .map(tag => tag.replace("|", "/"))
+                .map(tag => tag.replace(/\|/g, "/"))
                 .filter(p => getParentPath(p, false) == path),
         [allTags, pageTag],
     );
