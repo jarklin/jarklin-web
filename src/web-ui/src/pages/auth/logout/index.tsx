034e4672823ed895a733b18cc4f787f47ce9af11
playerg9 <g9yout@gmail.com>
2024-07-26 19:11:09 +0200

    fixed logout infinite requests bug

--- src/web-ui/src/pages/auth/logout/index.tsx
+++ src/web-ui/src/pages/auth/logout/index.tsx
@@ -17,8 +17,10 @@ export default function LogoutPage() {
     );
 
     useEffect(() => {
-        logout.mutate();
-    }, [logout]);
+        if (logout.isIdle) {
+            logout.mutate();
+        }
+    }, [logout.status]);
 
     return <div className="h-full grid place-content-center text-2xl gap-2 text-center">
         {logout.isError
