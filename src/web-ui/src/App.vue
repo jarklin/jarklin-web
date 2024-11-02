95706821f894cfdce4c2e2ea77a664047c05b972
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-02 11:05:58 +0100

    added automatic update if the document title based on the route

--- src/web-ui/src/App.vue
+++ src/web-ui/src/App.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+import {watch} from "vue";
+import {toTitleCase} from "@/lib/utils";
+
+const route = useRoute();
+
+watch(() => route.name as string, (newName) => {
+  if (newName) {
+    document.title = `Jarklin - ${toTitleCase(newName)}`;
+  } else {
+    document.title = `Jarklin`;
+  }
+})
+</script>
+
 <template>
   <router-view />
 </template>
