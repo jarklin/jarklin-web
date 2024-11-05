d788d1bb28e879082b3e8f799da67cb3d26cb9cf
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-05 21:48:01 +0100

    implemented ProblemsView.vue

---
+++ src/web-ui/src/views/panel/problems/ProblemsView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {useProblemsQuery} from "@/composables";
+import {Separator} from "@/components/ui/separator";
+
+const problemsQuery = useProblemsQuery();
+</script>
+
+<template>
+  <div v-if="problemsQuery.isPending"></div>
+  <p v-else-if="!problemsQuery.data!.length">
+    If some Media could be detected but not processed, it would be displayed here.
+    Lucky for you no problems were detected.
+  </p>
+  <template v-else>
+    <p>Some Media could not be processed. See here the details.</p>
+    <Separator />
+    <div class="grow flex flex-col gap-y-2">
+      <div v-for="problem in problemsQuery.data" class="group font-mono leading-none p-2 rounded-md text-sm flex flex-col gap-y-1 border border-input">
+        <p class="text-primary">{{ problem.file }}</p>
+        <Separator />
+        <details>
+          <summary class="cursor-pointer">{{ problem.type }}: {{ problem.description }}</summary>
+          <pre class="text-sm mt-1 border border-input bg-accent p-1 rounded-md overflow-x-scroll">{{ problem.traceback }}</pre>
+        </details>
+      </div>
+    </div>
+  </template>
+</template>
