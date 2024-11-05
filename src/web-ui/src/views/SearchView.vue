2dd9a5595f06596ce5bac2f40ace1b45748753ad
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-05 21:49:40 +0100

    implemented input for the search

--- src/web-ui/src/views/SearchView.vue
+++ src/web-ui/src/views/SearchView.vue
@@ -1,7 +1,31 @@
 <script setup lang="ts">
+import {LucideSearch} from "lucide-vue-next";
+import {Input} from "@/components/ui/input";
+import {ref} from "vue";
+import {useUrlSearchParams, watchDebounced} from "@vueuse/core";
 
+const urlSearchParams = useUrlSearchParams<{ query?: string }>("hash");
+
+const inputQuery = ref<string>(urlSearchParams.query ?? "");
+
+const filterQuery = ref<string>(urlSearchParams.query ?? "");
+
+watchDebounced(inputQuery, () => {
+  filterQuery.value = inputQuery.value;
+  urlSearchParams.query = inputQuery.value;
+}, { debounce: 300, maxWait: 5000 });
 </script>
 
 <template>
-  Search
+  <div class="max-w-screen-2xl mx-auto p-2">
+    <div class="max-w-2xl mx-auto relative items-center">
+      <Input v-model.trim="inputQuery" type="text" placeholder="Search..." class="pl-10" />
+      <span class="absolute start-0 inset-y-0 grid place-content-center px-2">
+        <LucideSearch class="size-6 text-muted-foreground" />
+      </span>
+    </div>
+    <div>
+      {{ filterQuery }}
+    </div>
+  </div>
 </template>
