f600aa6908b1eb8a448703562c327affb64cf7b7
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-05 10:14:34 +0100

    copied old Layout for panel

--- src/web-ui/src/views/panel/Layout.vue
+++ src/web-ui/src/views/panel/Layout.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
+import router from "@/router";
+import {Separator} from "@/components/ui/separator";
+import {toTitleCase} from "@/lib/utils";
 
+const subRoutes = router.getRoutes()
+  .filter(route => route.path.startsWith("/panel/") && !!route.name)
 </script>
 
 <template>
-  <div>
-    Panel
+  <div class="max-w-screen-lg mx-auto p-4">
+    <div class="justify-center flex flex-nowrap gap-x-2 md:gap-x-4">
+      <template v-for="(route, index) in subRoutes">
+        <Separator v-if="index !== 0" orientation="vertical" class="h-4 my-auto" />
+        <router-link :to="{ name: route.name }" class="group p-1 font-bold" active-class="[&>span]:max-w-full">
+          {{ toTitleCase(route.name as string) }}
+          <span class="block mx-auto max-w-0 group-hover:max-w-full transition-[max-width] h-px bg-primary" />
+        </router-link>
+      </template>
+    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
