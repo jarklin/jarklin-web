4bb960464cd658f3bdb65add32f544f3873f14d5
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-05 21:48:38 +0100

    implemented minimal SettingsView.vue with Logout-Button

--- src/web-ui/src/views/panel/settings/SettingsView.vue
+++ src/web-ui/src/views/panel/settings/SettingsView.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+import {LucideLogOut} from "lucide-vue-next";
+import {useApiConfigQuery} from "@/composables";
+import {Separator} from "@/components/ui/separator";
+import {Button} from "@/components/ui/button";
 
+const apiConfig = useApiConfigQuery();
 </script>
 
 <template>
-  Settings
+  <div class="grow" />
+  <template v-if="apiConfig.data?.requires_auth">
+    <Separator />
+    <router-link :to="{ name: 'logout' }" class="mx-auto">
+      <Button>
+        <LucideLogOut class="inline-block h-5" /> Logout
+      </Button>
+    </router-link>
+  </template>
 </template>
