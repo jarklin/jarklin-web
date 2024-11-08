6027d57e6db6a6953dbb3a982c9cd256bf9d6bcd
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-08 18:46:38 +0100

    added links to the homepage for explorer, tags and collections

--- src/web-ui/src/views/HomeView.vue
+++ src/web-ui/src/views/HomeView.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import {LucideFolderClosed, LucideLibraryBig, LucideTag} from "lucide-vue-next";
+import {Button} from "@/components/ui/button";
+import {Separator} from "@/components/ui/separator";
+</script>
+
 <template>
-  <main class="grid place-content-center">
-    Hello World
-  </main>
+  <div class="flex flex-wrap gap-2 justify-center p-2">
+    <router-link :to="{ name: 'explorer', params: { mediaPath: '' } }">
+      <Button variant="secondary" size="lg">
+        <LucideFolderClosed class="inline-block" /> Explorer
+      </Button>
+    </router-link>
+    <router-link :to="{ name: 'tags' }">
+      <Button variant="secondary" size="lg">
+        <LucideTag class="inline-block" /> Tags
+      </Button>
+    </router-link>
+    <router-link :to="{ name: 'collections' }">
+      <Button variant="secondary" size="lg">
+        <LucideLibraryBig class="inline-block" /> Collections
+      </Button>
+    </router-link>
+  </div>
+  <Separator />
 </template>
