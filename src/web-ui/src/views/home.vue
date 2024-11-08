cf44a170b6e75a2576024207a5ee881385132dac
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-08 19:13:42 +0100

    made the homepage more dynamic

--- src/web-ui/src/views/home.vue
+++ src/web-ui/src/views/home.vue
@@ -1,26 +1,49 @@
 <script setup lang="ts">
-import {LucideFolderClosed, LucideLibraryBig, LucideTag} from "lucide-vue-next";
+import {LucideFolderClosed, type LucideIcon, LucideLibraryBig, LucideTag} from "lucide-vue-next";
 import {Button} from "@/components/ui/button";
 import {Separator} from "@/components/ui/separator";
+import type {RouteLocationRaw} from "vue-router";
+
+interface HomepageElement {
+  displayName: string
+  location: RouteLocationRaw
+  icon?: LucideIcon
+  component?: string
+}
+
+const homepageElements: HomepageElement[] = [
+  {
+    displayName: "Explorer",
+    icon: LucideFolderClosed,
+    location: { name: 'explorer', params: { mediaPath: '' } },
+  },
+  {
+    displayName: "Tags",
+    icon: LucideTag,
+    location: { name: 'tags' },
+  },
+  {
+    displayName: "Collections",
+    icon: LucideLibraryBig,
+    location: { name: 'collections' },
+  },
+]
 </script>
 
 <template>
   <div class="flex flex-wrap gap-2 justify-center p-2">
-    <router-link :to="{ name: 'explorer', params: { mediaPath: '' } }">
+    <router-link v-for="element in homepageElements" :key="element.displayName" :to="element.location">
       <Button variant="secondary" size="lg">
-        <LucideFolderClosed class="inline-block" /> Explorer
-      </Button>
-    </router-link>
-    <router-link :to="{ name: 'tags' }">
-      <Button variant="secondary" size="lg">
-        <LucideTag class="inline-block" /> Tags
-      </Button>
-    </router-link>
-    <router-link :to="{ name: 'collections' }">
-      <Button variant="secondary" size="lg">
-        <LucideLibraryBig class="inline-block" /> Collections
+        <component v-if="element.icon" :is="element.icon" class="inline-block" />
+        {{ element.displayName }}
       </Button>
     </router-link>
   </div>
-  <Separator />
+  <template v-for="element in homepageElements" :key="element.displayName">
+    <template v-if="element.component">
+      <Separator />
+      <h1>{{ element.displayName }}</h1>
+      <component :is="element.component" />
+    </template>
+  </template>
 </template>
