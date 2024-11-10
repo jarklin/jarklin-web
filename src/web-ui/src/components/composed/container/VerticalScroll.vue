e405367f2dc167b76720e698d9f6abc2bd67b8ee
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-10 17:20:42 +0100

    added VerticalScroll container

---
+++ src/web-ui/src/components/composed/container/VerticalScroll.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex gap-x-2 md:gap-x-3 lg:gap-x-4 overflow-x-scroll">
+    <slot />
+  </div>
+</template>
---
+++ src/web-ui/src/components/composed/container/index.ts
@@ -0,0 +1,1 @@
+export { default as VerticalScroll } from "./VerticalScroll.vue";
