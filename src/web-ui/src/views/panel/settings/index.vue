1c2f7d12add47adde840ed114ca80068417d9293
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-24 09:29:34 +0100

    added a note to the settings page

--- src/web-ui/src/views/panel/settings/index.vue
+++ src/web-ui/src/views/panel/settings/index.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
-import {LucideLogOut} from "lucide-vue-next";
+import { LucideInfo, LucideLogOut } from "lucide-vue-next";
 import {useApiConfigQuery} from "@/composables";
 import {Separator} from "@/components/ui/separator";
 import {Button} from "@/components/ui/button";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 const apiConfig = useApiConfigQuery();
 </script>
 
 <template>
+  <Alert type="warning">
+    <LucideInfo class="size-4" />
+    <AlertTitle>
+      Settings are not synced
+    </AlertTitle>
+    <AlertDescription>
+      As of now. There exists no system of syncing these settings between devices or browsers.
+      This means that everything you configure here will only be applied to this specific browser and device.
+    </AlertDescription>
+  </Alert>
   <div class="grow" />
   <template v-if="apiConfig.data?.requires_auth">
     <Separator />
