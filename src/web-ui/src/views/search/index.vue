131234333169ba4654de1a949904ed590d4510b4
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-25 17:10:30 +0100

    implemented search filtering

--- src/web-ui/src/views/search/index.vue
+++ src/web-ui/src/views/search/index.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import {LucideSearch} from "lucide-vue-next";
 import {Input} from "@/components/ui/input";
-import {ref} from "vue";
+import { computed, ref } from "vue";
 import {onStartTyping, useUrlSearchParams, watchDebounced} from "@vueuse/core";
 import {MainLayout} from "@/layouts";
+import { useMediaQuery } from "@/composables";
+import * as JsSearch from "js-search";
+import { MediaCard } from "@/components/composed/mediacard";
+
+const mediaQuery = useMediaQuery();
 
 const urlSearchParams = useUrlSearchParams<{ query?: string }>("hash");
 
@@ -11,6 +16,18 @@ const inputQuery = ref<string>(urlSearchParams.query ?? "");
 const inputRef = ref();
 
 const filterQuery = ref<string>(urlSearchParams.query ?? "");
+
+const search = computed(() => {
+  const s = new JsSearch.Search("path");
+  s.addIndex("name");
+  s.addIndex("tags");
+  s.addDocuments(mediaQuery.data ?? []);
+  return s;
+});
+
+const matchingMedia = computed(
+    () => search.value.search(filterQuery.value) as typeof mediaQuery.data,
+);
 
 watchDebounced(inputQuery, () => {
   filterQuery.value = inputQuery.value;
@@ -25,15 +42,17 @@ onStartTyping(() => {
 </script>
 
 <template>
-  <MainLayout class="max-w-screen-2xl mx-auto p-2">
-    <div class="max-w-2xl mx-auto relative items-center">
+  <MainLayout class="flex flex-col gap-2 p-2">
+    <div class="w-full max-w-2xl mx-auto relative items-center">
       <Input v-model.trim="inputQuery" type="text" placeholder="Search..." class="pl-10" />
       <span class="absolute start-0 inset-y-0 grid place-content-center px-2">
         <LucideSearch class="size-6 text-muted-foreground" />
       </span>
     </div>
-    <div>
-      {{ filterQuery }}
+    <div class="flex flex-wrap gap-2">
+      <router-link v-for="media in matchingMedia" :key="media.path" :to="{ name: 'media-details', params: { mediaPath: media.path } }" class="h-80">
+        <MediaCard :media="media" />
+      </router-link>
     </div>
   </MainLayout>
 </template>
