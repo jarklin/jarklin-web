b2522eec140492ab775c990033ad0ad88f3238d0
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-29 00:25:50 +0100

    extended @/components

---
+++ src/web-ui/src/components/ui/setting/Setting.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib";
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>();
+</script>
+
+<template>
+  <div :class="cn('relative [&>button~*]:pl-8 [&>button]:absolute [&>button]:left-2', props.class)" role="group">
+    <slot />
+  </div>
+</template>
---
+++ src/web-ui/src/components/ui/setting/index.ts
@@ -0,0 +1,3 @@
+export { default as Setting } from './Setting.vue'
+export { default as SettingTitle } from './SettingTitle.vue'
+export { default as SettingDescription } from './SettingDescription.vue'
