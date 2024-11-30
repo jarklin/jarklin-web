7f875a012f48a788a2559e1065303893b2905de0
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-11-30 17:35:35 +0100

    fixed MasonryGrid.vue

--- src/web-ui/src/components/composed/container/MasonryGrid.vue
+++ src/web-ui/src/components/composed/container/MasonryGrid.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="grid gap-4 justify-evenly items-stretch place-content-center" style="grid-template-columns: repeat(auto-fit, minmax(200px, 1fr))">
+  <div class="grid gap-4 justify-evenly items-stretch place-content-center" style="grid-template-columns: repeat(auto-fill, minmax(200px, 1fr))">
     <slot />
   </div>
 </template>
