1569ff6f9e5a270302dd40c93c5b336a6537c700
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2024-12-15 18:47:09 +0100

    extended gallery media-details

--- src/web-ui/src/pages/media/details/_GalleryDetails.vue
+++ src/web-ui/src/pages/media/details/_GalleryDetails.vue
@@ -3,12 +3,30 @@ import * as humanize from "humanize-plus";
 import { Badge } from "@/components/ui/badge";
 import type { GalleryMediaEntry } from "@/types";
 import { useWebSettings } from "@/composables";
+import { type LucideIcon, LucideRectangleHorizontal, LucideRectangleVertical, LucideSquare } from "lucide-vue-next";
+import { computed } from "vue";
+import { findClosestEntry } from "@/lib";
 
-defineProps<{
+const { media } = defineProps<{
   media: GalleryMediaEntry
 }>();
 
 const extendedMediaDetails = useWebSettings("extendedMediaDetails");
+
+const aspect2IconMap: Record<number, [LucideIcon, string]> = {
+  [9/16]: [LucideRectangleVertical, "portrait"],
+  [1]: [LucideSquare, "squared"],
+  [16/9]: [LucideRectangleHorizontal, "landscape"],
+};
+
+const aspectIcons = computed(() => (
+    Array
+        .from(new Set(
+            media.meta.images
+                .map(image => image.width / image.height)  // calculate aspect-ratio's
+                .map(ratio => findClosestEntry(aspect2IconMap, ratio)),
+        ))
+));
 </script>
 
 <template>
@@ -28,6 +46,16 @@ const extendedMediaDetails = useWebSettings("extendedMediaDetails");
   </template>
   <label>Filetypes</label>
   <span class="flex flex-wrap gap-1">
-    <Badge v-for="ext in new Set(media.meta.images.map(img => img.ext))" :key="ext" variant="secondary">{{ ext }}</Badge>
+    <template v-for="ext in new Set(media.meta.images.map(img => img.ext))" :key="ext">
+      <Badge variant="secondary">{{ ext }}</Badge>
+    </template>
   </span>
+  <template v-if="extendedMediaDetails">
+    <label>Aspects</label>
+    <span>
+      <template v-for="[icon, label] in aspectIcons">
+        <component class="size-6" :is="icon" :title="label" />
+      </template>
+    </span>
+  </template>
 </template>
